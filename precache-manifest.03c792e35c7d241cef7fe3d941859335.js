2fd83093b2e28ecb7d7a64aaa54c0aed5a2b29c1
Leif T. Sundkvist <leif.t.sundkvist@gmail.com>
2020-04-08 00:35:18 +0200

    Updates

---
+++ precache-manifest.03c792e35c7d241cef7fe3d941859335.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65a0f9a680a80e7c2f6f9fea44b97de6",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "989da96f94ac5c871a9a",
+    "url": "/home/static/css/main.69365035.chunk.css"
+  },
+  {
+    "revision": "98730b5acfc0197fb3f6",
+    "url": "/home/static/js/2.b098a287.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/home/static/js/2.b098a287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "989da96f94ac5c871a9a",
+    "url": "/home/static/js/main.0cb071fc.chunk.js"
+  },
+  {
+    "revision": "1c545c851e5adf0e8ebf",
+    "url": "/home/static/js/runtime-main.a07717e5.js"
+  }
+]);
